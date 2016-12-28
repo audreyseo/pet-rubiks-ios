7aecacfabe70ca4cf077860f01f0b4e81cdf15fe
audreyseo <audreyleeseo@gmail.com>
2016-12-27 23:37:47 -0500

    Solved issues with timing
    It really had to do with the fact that I kept on adding to millis
    instead of just letting it be equal to the equivalent milliseconds of
    the difference between start time and the end time.
    
    So now it seems like it'll keep about the same place for at least a full
    minute, which is good.

--- cubeTimer/FirstViewController.swift
+++ cubeTimer/FirstViewController.swift
@@ -15,8 +15,8 @@ class FirstViewController: UIViewController {
 	@IBOutlet var timeLabel: UILabel!
 	var timer:Timer = Timer()
 	var millis:Int = 0
-	var isTiming:Bool = false
-	var timings:[Int] = [Int]()
+//	var isTiming:Bool = false
+//	var timings:[Int] = [Int]()
 	var longTouch:Bool = false
 	var t1:UInt64 = 0, t2:UInt64 = 0
 //	var link:CADisplayLink = CADisplayLink()
@@ -28,12 +28,8 @@ class FirstViewController: UIViewController {
 //		timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(FirstViewController.update), userInfo: nil, repeats: true)
 		self.view.backgroundColor = UIColor.black
 		scrambleLabel.textColor = UIColor.white
-		timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 87.0, weight: 0.05)
+		timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 87.0, weight: 0.02)
 		timeLabel.textColor = UIColor.white
-		
-//		link = CADisplayLink.init(target: self, selector: #selector(update))
-//		CADisplayLinkcreate
-		
 		
 	}
 
@@ -43,16 +39,14 @@ class FirstViewController: UIViewController {
 	}
 	@IBAction func startStopTimer(_ sender: UILongPressGestureRecognizer) {
 		if sender.state == .ended {
-			timer = Timer.scheduledTimer(timeInterval: 0.03, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+			timer = Timer.scheduledTimer(timeInterval: 0.04, target: self, selector: #selector(update), userInfo: nil, repeats: true)
 			t1 = mach_absolute_time()
-//				isTiming = !isTiming
-//			}
 		}
 	}
 
 	@IBAction func stopTimer(_ sender: Any) {
 		timer.invalidate()
-		timings += [millis]
+//		timings += [millis]
 		millis = 0
 	}
 	
@@ -72,7 +66,7 @@ class FirstViewController: UIViewController {
 		mach_timebase_info(&timeBaseInfo)
 		let elapsedNano = (t2 - t1) * UInt64(timeBaseInfo.numer) / UInt64(timeBaseInfo.denom);
 		
-		millis += Int(Float(elapsedNano) * 0.000001)
+		millis = Int(Float(elapsedNano) * 0.000001)
 		
 		let seconds = Int(Float(millis) / 1000.0)
 		let millisR = millis % 1000
@@ -80,7 +74,7 @@ class FirstViewController: UIViewController {
 		let millis0 = (millisR < 100) ? ((millisR < 10) ? "00" : "0") : ""
 //		print("Millis: ", millisR)
 		timeLabel.text = "\(seconds).\(millis0)\(millisR)"
-		t1 = t2
+//		t1 = t2
 		
 	}
 
