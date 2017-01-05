b05cd1a311ffd0ca3ee855fbd55caf859fdaccf3
audreyseo <audreyleeseo@gmail.com>
2017-01-05 11:28:27 -0500

    Resolved #39: test new fridrich cells
    I've tested most of them and also added them to the OLL VC. I haven't
    really done much with the PLL cells but I think that once I have the
    code down for the OLL one the PLL one should be fine.
    
    In fact, now that I think about it, I should have PLL subclass from OLL,
    and just change the values of the pertinent fields.

--- cubeTimer/CubeCaseCell.swift
+++ cubeTimer/CubeCaseCell.swift
@@ -27,16 +27,38 @@ class CubeCaseCell:UITableViewCell {
 		return label
 	}()
 	
+	let algLabel: UILabel = {
+		let label = UILabel()
+		label.text = "Sample Item"
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.font = UIFont.systemFont(ofSize: 14)
+		label.numberOfLines = 0
+		return label
+	}()
+	
+	let probLabel: UILabel = {
+		let label = UILabel()
+		label.text = "Sample Item"
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.font = UIFont.systemFont(ofSize: 10)
+		label.numberOfLines = 0
+		return label
+	}()
+	
 	func setupViews() {
 		addSubview(nameLabel)
+		addSubview(probLabel)
+		addSubview(algLabel)
 //		addSubview(statLabel)
 		//		addSubview(actionButton)
 		
 		//		actionButton.addTarget(self, action: "handleAction", forControlEvents: .TouchUpInside)
 		
 		
-		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-96-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-100-[v0]-[v2]-[v1]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": probLabel, "v2"
+			: algLabel]))
 		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
-//		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": statLabel]))
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": probLabel]))
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": algLabel]))
 	}
 }
--- cubeTimer/OLLViewController.swift
+++ cubeTimer/OLLViewController.swift
@@ -11,6 +11,66 @@ import UIKit
 class OLLViewController:UITableViewController {
 	var images:[String] = Array(repeating: "", count: 58)
 	
+	var caseInfo:[String: [String: String]] = [
+		"27": ["code": "OCLL1", "solve1alg": "(R U R' U) R U2 R'", "solve1length": "7", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"26": ["code": "OCLL2", "solve1alg": "R U2 R' U' R U' R'", "solve1length": "7", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"22": ["code": "OCLL3", "solve1alg": "[f (R U R' U') f'] [F (R U R' U') F']", "solve1length": "12", "solve2alg": "R U2' R2' U' R2 U' R2' U2 R2", "solve2length": "9", "prob": "\(1.0/54.0)"],
+		"21": ["code": "OCLL4", "solve1alg": "F (R U R' U') (R U R' U') (R U R' U') F'", "solve1length": "14", "solve2alg": "y (R' U' R) U' (R' U R) U' (R' U2 R)", "solve2length": "12", "prob": "\(1.0/108.0)"],
+		"24": ["code": "OCLL5", "solve1alg": "(r U R' U') (r' F R F')", "solve1length": "8", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"25": ["code": "OCLL6", "solve1alg": "F' (r U R' U') (r' F R)", "solve1length": "8", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"23": ["code": "OCLL7", "solve1alg": "R2 [D (R' U2) R] [D' (R' U2) R']", "solve1length": "9", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"58": ["code": "OCLL8", "solve1alg": "", "solve1length": "0", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/216.0)"],
+		"57": ["code": "E1", "solve1alg": "(R U R' U') M' (U R U' r')",  "solve1length": "9", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/108.0)"],
+		"28": ["code": "E2", "solve1alg": "M' U M U2 M' U M",  "solve1length": "7", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"44": ["code": "P1", "solve1alg": "f (R U R' U') f'", "solve1length": "6", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"43": ["code": "P2", "solve1alg": "f' (L' U' L U) f", "solve1length": "6", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"32": ["code": "P3", "solve1alg": "R U B' U' R' U R B R'", "solve1length": "9", "solve2alg": "R D L' D' R' U R B R'", "solve2length": "9", "prob": "\(1.0/54.0)"],
+		"31": ["code": "P4", "solve1alg": "R' U' F U R U' R' F' R", "solve1length": "9", "solve2alg": "y2 L' d' R d L U' L' B' L", "solve2length": "9", "prob": "\(1.0/54.0)"],
+		"38": ["code": "W1", "solve1alg": "(R U R' U) (R U' R' U') (R' F R F')", "solve1length": "12", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"36": ["code": "W2", "solve1alg": "(L' U' L U') (L' U L U) (L F' L' F)", "solve1length": "12", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"6": ["code": "S1", "solve1alg": "r U2 R' U' R U' r'", "solve1length": "7", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"5": ["code": "S2", "solve1alg": "r' U2 (R U R' U) r", "solve1length": "7", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"48": ["code": "L1", "solve1alg": "F (R U R' U') (R U R' U') F'", "solve1length": "10", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"47": ["code": "L2", "solve1alg": "F' (L' U' L U) (L' U' L U) F", "solve1length": "10", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"53": ["code": "L3", "solve1alg": "l' U' L U' L' U L U' L' U2 l", "solve1length": "11", "solve2alg": "y2 r' U' R U' R' U R U' R' U2 r", "solve2length": "12", "prob": "\(1.0/54.0)"],
+		"54": ["code": "L4", "solve1alg": "(r U R' U) R U' R' U R U2' r'", "solve1length": "11", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"49": ["code": "L5", "solve1alg": "(R' F R' F') R2 U2 y (R' F R F')", "solve1length": "11", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"50": ["code": "L6", "solve1alg": "R' F R2 B' R2' F' R2 B R'", "solve1length": "9", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"9": ["code": "F1", "solve1alg": "(R' U' R) y' x' (R U') (R' F) (R U R')", "solve1length": "12", "solve2alg": "(R U R' U')R' F R2 U R' U' F'", "solve2length": "11", "prob": "\(1.0/54.0)"],
+		"10": ["code": "F2", "solve1alg": "R U R' y R' F R U' R' F' R", "solve1length": "11", "solve2alg": "(R U R' U)(R' F R F') R U2 R'", "solve2length": "11", "prob": "\(1.0/54.0)"],
+		"35": ["code": "F3", "solve1alg": "(R U2 R') (R' F R F') (R U2 R')", "solve1length": "10", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"37": ["code": "F4", "solve1alg": "F R U' R' U' R U R' F'", "solve1length": "9", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"30": ["code": "A1", "solve1alg": "R2 U R' B' R U' R2 U R B R'", "solve1length": "11", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"29": ["code": "A2", "solve1alg": "(R U R' U') R U' R' F' U' F R U R'", "solve1length": "13", "solve2alg": "[F (R U R' U') F'] U2 [(R U R' U') (R' F R F')", "solve2length": "15", "prob": "\(1.0/54.0)"],
+		"41": ["code": "A3", "solve1alg": "[(R U R' U) R U2 R'] [F (R U R' U') F']", "solve1length": "13", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"42": ["code": "A4", "solve1alg": "[R' U2 (R U R' U) R] y [F (R U R' U') F']", "solve1length": "14", "solve2alg": "(R' F R F') (R' F R F') (R U R' U') (R U R')", "solve2length": "15", "prob": "\(1.0/54.0)"],
+		"7": ["code": "LB1", "solve1alg": "(r U R' U) R U2 r'", "solve1length": "7", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"8": ["code": "LB2", "solve1alg": "r' U' R U' R' U2 r", "solve1length": "7", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"11": ["code": "LB3", "solve1alg": "[F' (L' U' L U) F] y [F (R U R' U') F']", "solve1length": "13", "solve2alg": "y (r U R' U) (R' F R F') R U2 r'", "solve2length": "11", "prob": "\(1.0/54.0)"],
+		"12": ["code": "LB4", "solve1alg": "[F (R U R' U') F'] U [F (R U R' U') F']", "solve1length": "13", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"39": ["code": "LB5", "solve1alg": "R B' R' U' R U B U' R'", "solve1length": "9", "solve2alg": "y2 L F' (L' U' L U) F U' L'", "solve2length": "10", "prob": "\(1.0/54.0)"],
+		"40": ["code": "LB6", "solve1alg": "R' [F (R U R' U') F'] U R", "solve1length": "9", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"45": ["code": "T1", "solve1alg": "F (R U R' U') F'", "solve1length": "6", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"33": ["code": "T2", "solve1alg": "(R U R' U') (R' F R F')", "solve1length": "8", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"46": ["code": "C1", "solve1alg": "R' U' (R' F R F') U R", "solve1length": "8", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"34": ["code": "C2", "solve1alg": "(R U R2' U') (R' F) (R U) (R U') F'", "solve1length": "11", "solve2alg": "(R U R' U') x D' R' U R U' D x'", "solve2length": "12", "prob": "\(1.0/54.0)"],
+		"55": ["code": "I1", "solve1alg": "R' U2 R2 U R' U R U2 x' U' R' U", "solve1length": "12", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/108.0)"],
+		"52": ["code": "I2", "solve1alg": "(R U R' U) R d' R U' R' F'", "solve1length": "10", "solve2alg": "R' U' R U' R' d R' U R B", "solve2length": "10", "prob": "\(1.0/54.0)"],
+		"56": ["code": "I3", "solve1alg": "F (R U R' U') R F' (r U R' U') r'", "solve1length": "12", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/108.0)"],
+		"51": ["code": "I4", "solve1alg": "f (R U R' U') (R U R' U') f'", "solve1length": "10", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"16": ["code": "K1", "solve1alg": "(r U r') (R U R' U') (r U' r')", "solve1length": "10", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"15": ["code": "K2", "solve1alg": "(l' U' l) (L' U' L U) (l' U l)", "solve1length": "10", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"13": ["code": "K3", "solve1alg": "F U R U' R2 F' R (U R U' R')", "solve1length": "11", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"14": ["code": "K4", "solve1alg": "R' F R U R' F' R y' R U' R'", "solve1length": "11", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"1": ["code": "O1", "solve1alg": "R U2 R' (R' F R F') U2 (R' F R F')", "solve1length": "12", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/108.0)"],
+		"2": ["code": "O2", "solve1alg": "[F (R U R' U') F'] [f (R U R' U') f']", "solve1length": "12", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"3": ["code": "O3", "solve1alg": "[f (R U R' U') f'] U' [F (R U R' U') F']", "solve1length": "13", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"4": ["code": "O4", "solve1alg": "[f (R U R' U') f'] U [F (R U R' U') F']", "solve1length": "13", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"18": ["code": "O5", "solve1alg": "[F (R U R' U) F'] y' U2 (R' F R F')", "solve1length": "12", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"19": ["code": "O6", "solve1alg": "M U (R U R' U') M' (R' F R F')",  "solve1length": "11", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"17": ["code": "O7", "solve1alg": "(R U R' U) (R' F R F') U2 (R' F R F')", "solve1length": "13", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/54.0)"],
+		"20": ["code": "O8", "solve1alg": "M U (R U R' U') M2 (U R U' r')",  "solve1length": "11", "solve2alg": "", "solve2length": "0", "prob": "\(1.0/216.0)"]]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// do stuff here
@@ -25,6 +85,10 @@ class OLLViewController:UITableViewController {
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let myCell = tableView.dequeueReusableCell(withIdentifier: "caseCellId") as! CubeCaseCell
 		myCell.nameLabel.text = images[indexPath.row]
+		let str = (caseInfo[images[indexPath.row]]?["prob"]!)
+		let index = str?.index((str?.startIndex)!, offsetBy: 5)
+		myCell.probLabel.text = str?.substring(to: index!)
+		myCell.algLabel.text = (caseInfo[images[indexPath.row]]?["solve1alg"]!)
 		let image = UIImage(named: images[indexPath.row])
 		myCell.imageView?.image = image
 		
