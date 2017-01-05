3f3fe3fb5b83d428e86f889f8d8202e260b7fdfe
audreyseo <audreyleeseo@gmail.com>
2017-01-05 09:49:45 -0500

    Resolves #3, Resolves #1
    Issue #3 - Add tableview for PLL cases
    Issue #1 - Add tableview for OLL cases
    
    I decided to lump these two together because they're so similar. I've
    made two extra view controllers for each of their respective cases as
    well as subclassed UITableVC twice so that I could have a custom VC for
    each of them. They'll be basically the same besides the fact that they
    are for different VCs.
    
    I'm also contemplating subclassing UITableVC so that I can just make a
    few things uniform, i.e. header height/background color, table
    background color, etc. It would just take a lot of overhead off of both
    me and the code.

---
+++ cubeTimer/OLLViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OLLViewController.swift
+//  cubeTimer
+//
+//  Created by Audrey Seo on 5/1/2017.
+//  Copyright © 2017 Audrey Seo. All rights reserved.
+//
+
+import UIKit
+
+class OLLViewController:UITableViewController {
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// do stuff here
+	}
+}
---
+++ cubeTimer/PLLViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PLLViewController.swift
+//  cubeTimer
+//
+//  Created by Audrey Seo on 5/1/2017.
+//  Copyright © 2017 Audrey Seo. All rights reserved.
+//
+
+import UIKit
+
+
+class PLLViewController:UITableViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// do other stuff here
+	}
+}
