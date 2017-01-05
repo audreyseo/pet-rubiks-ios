e2f684122eb66cce8b2bb79669d8ca221979f73f
audreyseo <audreyleeseo@gmail.com>
2017-01-05 09:57:04 -0500

    Resolves #18: new view controller for editing
    In addition to adding a new view controller to the main storyboard, I
    also made classes for the respective VCs for editing and for choosing
    sessions. I also made sure to give the segues their own identifiers and
    identified the classes of each of the VCs in the storyboard, so that it
    might be easier in the future to tell them apart.

---
+++ cubeTimer/SessionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SessionViewController.swift
+//  cubeTimer
+//
+//  Created by Audrey Seo on 5/1/2017.
+//  Copyright © 2017 Audrey Seo. All rights reserved.
+//
+
+import UIKit
+
+class SessionViewController:UITableViewController {
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// do stuff here
+	}
+}
