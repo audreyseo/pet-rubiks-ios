64484d613af2f809803acfed0926c20106c96c77
audreyseo <audreyleeseo@gmail.com>
2017-01-05 10:21:03 -0500

    Resolved #35: add clear times functionality
    I added the necessary workings in order to clear all of one's times,
    including another alert to make sure you really want to do that.
    
    In the process, I discovered that it won't update SecondViewController
    unless you add Another time to the mix, so I need to fix that soon.

--- cubeTimer/TimesEditorViewController.swift
+++ cubeTimer/TimesEditorViewController.swift
@@ -9,13 +9,17 @@
 import UIKit
 
 class TimesEditorViewController:UITableViewController {
-	
+	var tbc:InfoSharingTabController? = InfoSharingTabController()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// do stuff here
 		navigationItem.title = "Times Editor"
 		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Options", style: .plain, target: self, action: #selector(showEditingActions))
+		
+		if self.tabBarController != nil {
+			self.tbc = (self.tabBarController as! InfoSharingTabController)
+		}
 	}
 	
 	
@@ -23,6 +27,15 @@ class TimesEditorViewController:UITableViewController {
 		let alert = UIAlertController(title: "Editing Action", message: "Which action would you like to take?", preferredStyle: .actionSheet)
 		let clearAction = UIAlertAction(title: "Clear Times", style: .default, handler: { action in
 				print("About to clear all items")
+			let alert = UIAlertController(title: "Clear All Times From Session", message: "Are you sure? This action cannot be undone.", preferredStyle: .alert)
+			let yesAction = UIAlertAction(title: "Yes", style: .destructive, handler: { action in
+					print("Clearing times.")
+				self.tbc?.data = [Int]()
+			})
+			let noAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
+			alert.addAction(yesAction)
+			alert.addAction(noAction)
+			self.present(alert, animated: true, completion: nil)
 		})
 		
 		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
