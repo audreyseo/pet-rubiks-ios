20cff213e5bd266053e785a792cb1cced22c2eaa
audreyseo <audreyleeseo@gmail.com>
2017-01-05 10:03:32 -0500

    Resolves #17: add clear times button to edit scene
    I actually added it in the form of an action sheet that you access by
    clicking on a button in the top right-hand corner called "Options" but I
    still added it. I haven't been able to test it because I haven't exactly
    put together the whole segue thing but I'm sure I'll get around to that
    as soon as I hook it up in SecondViewController, in the same function
    where I deal with all of the sharing stuff.

--- cubeTimer/TimesEditorViewController.swift
+++ cubeTimer/TimesEditorViewController.swift
@@ -14,5 +14,21 @@ class TimesEditorViewController:UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// do stuff here
+		navigationItem.title = "Times Editor"
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Options", style: .plain, target: self, action: #selector(showEditingActions))
+	}
+	
+	
+	func showEditingActions() {
+		let alert = UIAlertController(title: "Editing Action", message: "Which action would you like to take?", preferredStyle: .actionSheet)
+		let clearAction = UIAlertAction(title: "Clear Times", style: .default, handler: { action in
+				print("About to clear all items")
+		})
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		
+		alert.addAction(clearAction)
+		alert.addAction(cancelAction)
+		
 	}
 }
