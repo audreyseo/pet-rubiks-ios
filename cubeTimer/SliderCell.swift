d8801e67e17cf14258565d1f6aed27cbe2d8d3a3
audreyseo <audreyleeseo@gmail.com>
2017-01-06 11:37:53 -0500

    Resolved #60 - create slider cell
    I made a SliderCell class. In addition to the normal attributes of
    nameLabel, it also has valueLabel and cellSlider. cellSlider is a
    UISlider that can be used to set some value in settings. In this case,
    it is 0.5 by default. Setting the user's personal default will be a job
    for the SettingsVC.
    
    I have not tested the SliderCell yet but it should update on its own.

---
+++ cubeTimer/SliderCell.swift
@@ -0,0 +1,71 @@
+//
+//  SliderCell.swift
+//  cubeTimer
+//
+//  Created by Audrey Seo on 6/1/2017.
+//  Copyright © 2017 Audrey Seo. All rights reserved.
+//
+
+import UIKit
+
+class SliderCell:UITableViewCell {
+	//	var myTableViewController: SecondViewController?
+	
+	override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		setupViews()
+	}
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	let nameLabel: UILabel = {
+		let label = UILabel()
+		label.text = "Sample Item"
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.font = UIFont.systemFont(ofSize: 14)
+		label.numberOfLines = 0
+		return label
+	}()
+	
+	let valueLabel: UILabel = {
+		let label = UILabel()
+		label.text = "0.5"
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.font = UIFont.systemFont(ofSize: 12)
+		return label
+	}()
+	
+	let cellSlider: UISlider = {
+		let sl = UISlider()
+		sl.translatesAutoresizingMaskIntoConstraints = false
+		sl.isContinuous = true
+		sl.minimumValue = 0.1
+		sl.maximumValue = 5.0
+		sl.setValue(0.5, animated: true)
+		sl.addTarget(self, action: #selector(setLabel), for: .allTouchEvents)
+		return sl
+	}()
+	
+	func setSlider(newValue: Float) {
+		cellSlider.setValue(newValue, animated: true)
+	}
+	
+	func setLabel() {
+		let raw:Float = cellSlider.value
+		let rawStr:String = "\(raw)"
+		valueLabel.text = rawStr.substring(to: rawStr.index(rawStr.startIndex, offsetBy: 5, limitedBy: rawStr.endIndex))
+	}
+	
+	func setupViews() {
+		addSubview(nameLabel)
+		addSubview(valueLabel)
+		addSubview(cellSlider)
+		
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-[v1]-[v2]-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": valueLabel, "v2": cellSlider]))
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": valueLabel]))
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": cellSlider]))
+	}
+}
+
