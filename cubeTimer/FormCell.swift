62120da4d0b774a063bc47ae5ba6b3a956e5d342
audreyseo <audreyleeseo@gmail.com>
2017-01-07 07:32:07 -0500

    Resolves #80 - form cell type enum
    Made a form cell type enum with four different types
     - fill in the blank
     - pick a date
     - pick a something (with default)
     - pick something (without default)
    
    I'll prune these along the way if it turns out that there's something
    that is unnecessary.

---
+++ cubeTimer/FormCell.swift
@@ -0,0 +1,17 @@
+//
+//  FormCell.swift
+//  cubeTimer
+//
+//  Created by Audrey Seo on 6/1/2017.
+//  Copyright © 2017 Audrey Seo. All rights reserved.
+//
+
+import UIKit
+
+enum FormCellType {
+	case fillInTheBlank, pickDate, pickerWithDefault, pickerWithoutDefault
+}
+
+class FormCell:UITableViewCell {
+	
+}
