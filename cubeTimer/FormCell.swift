e4a316a6b43c247a131058182ea5d4cba0dfe2f6
audreyseo <audreyleeseo@gmail.com>
2017-01-07 09:24:06 -0500

    Resolves #81 - form cell class
    I've added functions to make it easier for me to deal with things - such
    as adding constraints. I have not implemented most of it yet, but to
    start off, I just made a basic one that has its name but also has a
    variable for a UITextField so that I can add it in the future.

--- cubeTimer/FormCell.swift
+++ cubeTimer/FormCell.swift
@@ -13,5 +13,40 @@ enum FormCellType {
 }
 
 class FormCell:UITableViewCell {
+	let nameLabel:UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.font = UIFont.systemFont(ofSize: 14.0)
+		label.numberOfLines = 0
+		return label
+	}()
 	
+	let blank:UITextField = {
+		let text = UITextField()
+		text.translatesAutoresizingMaskIntoConstraints = false
+		text.font = UIFont.systemFont(ofSize: 13.0)
+		text.placeholder = "Placeholder Text"
+		return text
+	}()
+	
+	override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder) has not been implemented")
+	}
+	
+	func myConstraints(vf:String, views: [String: Any]) {
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vf, options: NSLayoutFormatOptions(), metrics: nil, views: views))
+	}
+	
+	func setupViews() {
+		addSubview(nameLabel)
+		
+		myConstraints(vf: "H:|-16-[v0]-16-|", views: ["v0": nameLabel])
+		myConstraints(vf: "V:|-4-[v0]-4-|", views: ["v0": nameLabel])
+//		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+//		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-4-[v0]-4-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+	}
 }
