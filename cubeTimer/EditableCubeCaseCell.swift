5b64a9570d20ebf8fea3486f7194e80a1f0ae3b7
audreyseo <audreyleeseo@gmail.com>
2017-01-21 17:18:24 -0500

    Added constraints to EditableCubeCaseCell
    Mostly just copied and pasted them.

--- cubeTimer/EditableCubeCaseCell.swift
+++ cubeTimer/EditableCubeCaseCell.swift
@@ -26,11 +26,18 @@ class EditableCubeCaseCell: CubeCaseCell {
 		self.contentView.addSubview(algStacker)
 		self.contentView.addSubview(probLabel)
 		
+		let viewsDict: [String: Any] = ["v0": imageNameStack, "v1": probLabel, "v2": algStacker]
+		
 		self.setupImageStackView()
 		self.setupAlgorithmStackView()
 		
 		
 		// Add constraints to the cell's view
+		addLayoutConstraint(format: "H:|-8-[v0(60)]-4-[v2]->=4-[v1(>=20,<=35)]-8-|", viewsDictionary: viewsDict)
+		addLayoutConstraint(format: "V:|-8-[v0(70)]-8-|", viewsDictionary: viewsDict)
+		addLayoutConstraint(format: "V:|-8-[v1]-8-|", viewsDictionary: viewsDict)
+		addLayoutConstraint(format: "V:|-8-[v2]-8-|", viewsDictionary: viewsDict)
 		
+		addConstraint(equalityConstraint(item: algStacker, other: self, attributeToUse: .centerY))
 	}
 }
