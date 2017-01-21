a602a9e29b8146539fff5bd21a08757261263cdb
audreyseo <audreyleeseo@gmail.com>
2017-01-21 16:28:40 -0500

    Deleted old code/comments from CubeCaseCell
    If I'm going to work on the positioning of elements when editing a case
    cell, I want to sort of start off with a fresh slate, so to speak.
    
    There were so many old comments, I could barely tell what was going on
    in the code, so I'm probably going to be just refactoring for the next
    few commits, including this one.

--- cubeTimer/CubeCaseCell.swift
+++ cubeTimer/CubeCaseCell.swift
@@ -13,7 +13,6 @@ class CubeCaseCell:UITableViewCell {
 	override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
 		self.imageView?.translatesAutoresizingMaskIntoConstraints = false
-//		setupViews()
 	}
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
@@ -83,41 +82,26 @@ class CubeCaseCell:UITableViewCell {
 	}()
 	
 	override func layoutSubviews() {
-		// This combination kind of works - as in everything is the right size
-		// it's just that the image view doesn't show up.
 		super.layoutSubviews()
 		self.imageView?.removeFromSuperview()
-		
-		
-		
-//		self.separatorInset = UIEdgeInsets(top: 0, left: 55 + 16, bottom: 0, right: 0)
-//		self.separatorStyle = .singleLine
-//		super.layoutSubviews()
-		// Do nothing
 	}
 	
 	func setupViews() {
 		self.separatorInset = UIEdgeInsets(top: 0, left: 55 + 16, bottom: 0, right: 0)
-//		super.layoutSubviews()
-//		self.imageView?.removeFromSuperview()
 		self.separatorInset = UIEdgeInsets(top: 0, left: 55 + 16, bottom: 0, right: 0)
 		
-		//algStacker.alignment = .center
-		//algStacker.axis = .horizontal
 		if algLabel.text != "" {
 			algLabel.removeFromSuperview()
 			algLabel1.removeFromSuperview()
 			
 			algStacker.addSubview(algLabel)
 			if algLabel1.text != "" {
-//				print("\(algLabel.text)-\(algLabel1.text): Double")
 				algStacker.addSubview(algLabel1)
 				algStacker.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[v0]-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": algLabel]))
 				algStacker.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[v0]-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": algLabel1]))
 				algStacker.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]->=4-[v1]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": algLabel, "v1": algLabel1]))
 				
 			} else {
-//				print("\(algLabel.text)-\(algLabel1.text): Single.")
 				algStacker.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[v0]-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": algLabel]))
 				algStacker.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|->=27-[v0]->=27-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": algLabel]))
 			}
@@ -132,32 +116,14 @@ class CubeCaseCell:UITableViewCell {
 		imageNameStack.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[v0(50)]-[v1(<=15)]-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": self.customImageView, "v1": self.nameLabel]))
 		
 		
-		
-//		let sepView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width - 55, height: 1))
-//		sepView.backgroundColor = UIColor.gray
-		
-//		addSubview(sepView)
-		
 		addSubview(imageNameStack)
-//		addSubview(nameLabel)
 		addSubview(probLabel)
 		addSubview(algStacker)
-		//addSubview(algLabel)
-		
-		
-//		addSubview(statLabel)
-		//		addSubview(actionButton)
-		
-		//		actionButton.addTarget(self, action: "handleAction", forControlEvents: .TouchUpInside)
 		
 		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0(60)]-4-[v2]->=4-[v1(>=20,<=35)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": imageNameStack, "v1": probLabel, "v2"
 			: algStacker]))
-//		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-55-[v0]-0-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": sepView]))
-		//		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-85-[v0(>=15)]-4-[v2]->=4-[v1(>=20,<=35)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": probLabel, "v2"
-		//			: algStacker]))
 		addConstraint(NSLayoutConstraint(item: algStacker, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: 0.0))
 		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0(70)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": imageNameStack]))
-//		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
 		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": probLabel]))
 		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": algStacker]))
 	}
